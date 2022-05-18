3253974126148746ec6a517207b2fea2b2a1eff4
preetisrivastav <52635600+preetisrivastav@users.noreply.github.com>
2022-05-17 23:45:08 -0700

    Update streamlit_app.py
    added json

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+streamlit.header('Fruityvice Fruit Advice!')
 import requests
 
 streamlit.title('My Parents New Healthy Diner')
@@ -17,4 +18,4 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
